d9d5fdfcf56afdda441018b49a2b12de4fc85f1e
rodrigo <rodrigomoyanocat@gmail.com>
2021-11-25 18:50:55 -0300

    commit j

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -84,7 +84,7 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  num<90=true;
+  
 }
 
 function mayorQueCincuenta(num) {
