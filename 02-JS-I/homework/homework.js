7f46a2eab3a25d51918ebf5aff357972bd1a0c63
rodrigo <rodrigomoyanocat@gmail.com>
2021-11-25 16:44:41 -0300

    commi 3

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'este es un string';
 const nuevoNum = 123;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
