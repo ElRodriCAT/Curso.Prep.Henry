724a494271cfd370d2e06ebd106dbdf368de299a
rodrigo <rodrigomoyanocat@gmail.com>
2021-11-26 14:33:33 -0300

    fizz3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -121,15 +121,13 @@ function fizzBuzz(numero) {
   if(numero/3===0){
     return "fizz";
   }
-  else if(numero/5===0){
+  if(numero/5===0){
     return "buzz";
   }
-  else if(numero/3===0 && numero/5===0){
-    return "fizzbuzz"
+  if(numero/3===0 && numero/5===0){
+    return "fizzbuzz";
   }
-  else {
     return numero
-  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
