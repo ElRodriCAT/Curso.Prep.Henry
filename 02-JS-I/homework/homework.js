afe28a9712cfc549454bd12322adc548268c5bfe
rodrigo <rodrigomoyanocat@gmail.com>
2021-11-25 21:16:41 -0300

    102sd

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -190,17 +190,15 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if(numero===0){
-    return false;
-  }
-  else{
-    if(numero>0){
-      return "es positivo";
-     }
-    else{
-    return "es negativo";
-    }
-  }
+ if(numero === 0){
+   return false
+ }
+ else if(numero>0){
+return "es positivo"
+ }
+ else{
+   return "es negativo"
+ }
 }
 
 function agregarSimboloExclamacion(str) {
